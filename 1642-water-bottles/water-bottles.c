b88acd3e5ee8afbeae8b4e8ae3f4201b61789292
pragatipat2006-cmd <pragatipat2006@gmail.com>
2026-06-20 20:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (4.66%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.c
@@ -0,0 +1,14 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int total = numBottles;
+    int empty = numBottles;
+
+    while (empty >= numExchange) {
+        int newBottles = empty / numExchange;
+
+        total += newBottles;
+
+        empty = newBottles + (empty % numExchange);
+    }
+
+    return total;
+}
